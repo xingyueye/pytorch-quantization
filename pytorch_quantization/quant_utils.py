fd15af4825a7e149b8d3e73b988a2257c673c9e5
liqingyuan02 <liqingyuan02@meituan.com>
2022-09-29 11:56:36 +0800

    add quant_utils.py

---
+++ pytorch_quantization/quant_utils.py
@@ -0,0 +1,40 @@
+from pytorch_quantization import nn as quant_nn
+
+def set_module(model, submodule_key, module):
+    tokens = submodule_key.split('.')
+    sub_tokens = tokens[:-1]
+    cur_mod = model
+    for s in sub_tokens:
+        cur_mod = getattr(cur_mod, s)
+    setattr(cur_mod, tokens[-1], module)
+
+def get_module(model, submodule_key):
+    sub_tokens = submodule_key.split('.')
+    cur_mod = model
+    for s in sub_tokens:
+        cur_mod = getattr(cur_mod, s)
+    return cur_mod
+
+def module_quant_disable(model, k):
+    cur_module = get_module(model, k)
+    if hasattr(cur_module, '_input_quantizer'):
+        cur_module._input_quantizer.disable()
+    if hasattr(cur_module, '_weight_quantizer'):
+        cur_module._weight_quantizer.disable()
+
+def module_quant_enable(model, k):
+    cur_module = get_module(model, k)
+    if hasattr(cur_module, '_input_quantizer'):
+        cur_module._input_quantizer.enable()
+    if hasattr(cur_module, '_weight_quantizer'):
+        cur_module._weight_quantizer.enable()
+
+def model_quant_disable(model):
+    for name, module in model.named_modules():
+        if isinstance(module, quant_nn.TensorQuantizer):
+            module.disable()
+
+def model_quant_enable(model):
+    for name, module in model.named_modules():
+        if isinstance(module, quant_nn.TensorQuantizer):
+            module.enable()
