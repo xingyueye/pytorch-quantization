8335569c55bb73505effb016d74b2dcda380e3d0
liqingyuan02 <liqingyuan02@meituan.com>
2022-11-22 15:15:04 +0800

    add graph visualization of TensorRT engine

---
+++ examples/deploy/graph_draw.py
@@ -0,0 +1,20 @@
+# python3.7+
+import os
+import sys
+module_path = os.path.abspath(os.path.join(''))
+from trex import *
+
+
+if __name__ == '__main__':
+    engine_json_path = sys.argv[1]
+    engine_json_name = os.path.basename(engine_json_path)
+    assert engine_json_name is not None and engine_json_name[-5:] == '.json', "Not valid {} file".format(engine_json_name)
+
+    set_wide_display()
+    plan = EnginePlan(engine_json_name)
+
+    df = plan.df
+    print(df['Name'])
+
+    graph = to_dot(plan, layer_type_formatter)
+    svg_name = render_dot(graph, engine_json_name[:-5], 'svg')
