efa952b3ffeba7831eb07404c3d252ec41eede51
liqingyuan02 <liqingyuan02@meituan.com>
2022-12-22 11:09:00 +0800

    modify inserted quantizer's name from input_quantizer to _input_quantizer

--- pytorch_quantization/graph/fx_matcher.py
+++ pytorch_quantization/graph/fx_matcher.py
@@ -25,7 +25,7 @@ class ConvBnResReluTypePatternMatcher(PatternMatcher):
                 # Add quantizer to one edge of the residual add
                 print('node: ', node, node.args[0].name)
 
-                res_add_quantizer_name = F"{'.'.join(node.args[0].name.split('.'))}.input_quantizer"
+                res_add_quantizer_name = F"{'.'.join(node.args[0].name.split('.'))}._input_quantizer"
                 res_add_quantizer = TensorQuantizer(quantizer_desc)
                 model_traced.add_submodule(res_add_quantizer_name, res_add_quantizer)
                 # The matched end node is ReLU, whose args[0] is the add node we want to add quantizer to
@@ -45,14 +45,14 @@ class SESiLUTypePatternMatcher(PatternMatcher):
                 # Add quantizer to one edge of the residual mul
                 print('node: ', node, node.name, node.args[1].name)
 
-                res_mul_quantizer_name = F"{'.'.join(node.name.split('.'))}.input_quantizer"
+                res_mul_quantizer_name = F"{'.'.join(node.name.split('.'))}._input_quantizer"
                 res_mul_quantizer = TensorQuantizer(quantizer_desc)
                 model_traced.add_submodule(res_mul_quantizer_name, res_mul_quantizer)
                 # The matched end node is Mul, whose 1st input we want to add quantizer to
                 fx_utils.add_quantizer(node, model_traced, (0,), (res_mul_quantizer_name,))
 
                 # insert quantizer before sigmoid
-                sigmoid_quantizer_name = F"{'.'.join(node.args[1].name.split('.'))}.input_quantizer"
+                sigmoid_quantizer_name = F"{'.'.join(node.args[1].name.split('.'))}._input_quantizer"
                 sigmoid_quantizer = TensorQuantizer(quantizer_desc)
                 model_traced.add_submodule(sigmoid_quantizer_name, sigmoid_quantizer)
                 # node.args[1] is sigmoid node, whose 1st input we want to add quantizer to
@@ -71,7 +71,7 @@ class DropActDropPathAddTypePatternMatcher(PatternMatcher):
             if fx_utils.end_node_a_matches_graph_b_types(node, model_traced, self.pattern_graph, self.pattern_traced):
                 print('node: ', node, node.name)
 
-                res_add_quantizer_name = F"{'.'.join(node.name.split('.'))}.input_quantizer"
+                res_add_quantizer_name = F"{'.'.join(node.name.split('.'))}._input_quantizer"
                 res_add_quantizer = TensorQuantizer(quantizer_desc)
                 model_traced.add_submodule(res_add_quantizer_name, res_add_quantizer)
                 # The matched end node is Add, whose snd input we want to add quantizer to
@@ -89,7 +89,7 @@ class MeanTypePatternMatcher(PatternMatcher):
         for node in model_traced.graph.nodes:
             if fx_utils.end_node_a_matches_graph_b_types(node, model_traced, self.pattern_graph, self.pattern_traced):
                 print('node: ', node, node.name)
-                mean_quantizer_name = F"{'.'.join(node.name.split('.'))}.input_quantizer"
+                mean_quantizer_name = F"{'.'.join(node.name.split('.'))}._input_quantizer"
                 mean_quantizer = TensorQuantizer(quantizer_desc)
                 model_traced.add_submodule(mean_quantizer_name, mean_quantizer)
 
