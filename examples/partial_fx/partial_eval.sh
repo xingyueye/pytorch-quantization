9ee6d1cc511d715e3b99fdfa2be6cd32ab040475
liqingyuan02 <liqingyuan02@meituan.com>
2023-01-08 02:27:50 +0800

    save TensorRT engine at evaluation

--- examples/partial_fx/partial_eval.sh
+++ examples/partial_fx/partial_eval.sh
@@ -1,8 +1,13 @@
 ONNX_DIR=$1
 LOG_DIR=$2
+ENGINE_DIR=$3
 if [ ! -d ${LOG_DIR} ]; then
   echo "mkdir ${LOG_DIR}"
   mkdir ${LOG_DIR}
+fi
+if [ ! -d ${ENGINE_DIR} ]; then
+  echo "mkdir ${ENGINE_DIR}"
+  mkdir ${ENGINE_DIR}
 fi
 ONNX_FILES=$(ls $ONNX_DIR)
 for onnx_file in ${ONNX_FILES}
@@ -10,6 +15,6 @@ do
   # check suffix is onnx
   if [ "${onnx_file##*.}"x = "onnx"x ]; then
     echo "build ${ONNX_DIR}/${onnx_file}"
-    /usr/local/TensorRT-release/bin/trtexec --streams=1 --workspace=1024 --int8 --fp16 --inputIOFormats=fp16:chw --onnx=$ONNX_DIR/${onnx_file} 2>&1 > ${LOG_DIR}/${onnx_file%.*}_trt_build_log.txt
+    /usr/local/TensorRT-release/bin/trtexec --streams=1 --workspace=1024 --int8 --fp16 --inputIOFormats=fp16:chw --onnx=$ONNX_DIR/${onnx_file} --saveEngine=${ENGINE_DIR}/${onnx_file%.*}_int8.trt 2>&1 > ${LOG_DIR}/${onnx_file%.*}_trt_build_log.txt
   fi
 done
