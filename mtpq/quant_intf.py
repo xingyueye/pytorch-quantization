cac8dada3e62008c465db4b3d2b5388894e63d20
liliang58 <liliang58@meituan.com>
2023-02-27 21:39:19 +0800

    feat: add unsinged keywords

--- mtpq/quant_intf.py
+++ mtpq/quant_intf.py
@@ -120,7 +120,8 @@ def compute_amax(model, **kwargs):
 def get_quant_desc(config):
     quant_desc = {
         "input_desc": QuantDescriptor(num_bits=config.a_qscheme.bit, calib_method=config.a_qscheme.calib_method,
-                                                 quantizer_type=config.a_qscheme.quantizer_type),
+                                                 quantizer_type=config.a_qscheme.quantizer_type,
+                                                 unsigned=config.a_qscheme.unsigned if hasattr(config.a_qscheme, 'unsigned') else False),
         "conv_weight_desc": QuantDescriptor(num_bits=config.w_qscheme.bit, axis=(0) if config.w_qscheme.per_channel is True else None,
                                             calib_method=config.w_qscheme.calib_method,
                                             quantizer_type=config.w_qscheme.quantizer_type),
