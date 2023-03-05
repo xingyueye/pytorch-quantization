1e8a4ab3316a700514f5fa80b520aaa085739604
zhangbo97 <zhangbo97@meituan.com>
2023-03-05 16:17:54 +0800

    feat: add export onnx

--- mtpq/model_quantizer.py
+++ mtpq/model_quantizer.py
@@ -64,6 +64,13 @@ class ModelQuantizer:
     def export(self):
         pass
 
+    def export_onnx(self, data_shape, onnx_path='./', dynamic_axes=None):
+        onnx_name = self.calib_weights.replace(".pt", ".onnx") if dynamic_axes is None else self.calib_weights.replace(".pt", "_dynamic.onnx")
+        onnx_path = os.path.join(onnx_path, onnx_name)
+        quant_model_export(self.model, onnx_path, data_shape, dynamic_axes=dynamic_axes)
+        logger.info("Export QAT models with QDQ nodes as {}".format(onnx_path))
+        remove_qdq_nodes_from_qat_onnx(onnx_path)
+
 
 class TimmModelQuantizer(ModelQuantizer):
     def __init__(self, model_name, model, config, calib_weights='', save_ori_model=False):
